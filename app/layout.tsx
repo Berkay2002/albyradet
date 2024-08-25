4e05fb54f702ad6505663e4f559640e312ff6636
Berkay Orhan <beror658@student.liu.se>
2024-08-25 19:06:12 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,35 +1,33 @@
-// app/layout.tsx
+"use client";
+
 import * as React from 'react';
-import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
 import NavBar from './navbar';
 import MuiProvider from './MuiProvider';
 import Footer from './Footer';
 import { MobileStateProvider } from './MobileContext';
+import { UserProvider } from '@auth0/nextjs-auth0/client';
 
 const inter = Inter({ subsets: ['latin'] });
 
-export default function RootLayout({
-  children,
-}: {
+export default function RootLayout({ children }: {
   children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <MuiProvider>
-          <MobileStateProvider> 
-            <>
-
-              <NavBar />
-              {children}
-              <Footer />
-
-            </>
-
-          </MobileStateProvider>
-        </MuiProvider>
+        <UserProvider>
+          <MuiProvider>
+            <MobileStateProvider> 
+              <>
+                <NavBar />
+                {children}
+                <Footer />
+              </>
+            </MobileStateProvider>
+          </MuiProvider>
+        </UserProvider>
       </body>
     </html>
   );
